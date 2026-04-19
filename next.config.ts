3ce7b4e0a6a9d3d10e69907602a7d4b6144b7939
xavier <francoiswakegbandi@gmail.com>
2026-04-19 21:14:17 +0000

    modifications1

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
 }
+};
+
+
 
 
 export default nextConfig;
